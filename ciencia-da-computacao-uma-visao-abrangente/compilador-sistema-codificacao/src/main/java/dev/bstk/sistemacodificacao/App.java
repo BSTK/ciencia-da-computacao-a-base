e7a20f11582d4736a1e0f346798b5af73a5a3719
Bruno <bsilva.se@gmail.com>
2023-09-12 13:37:07 -0300

    main - Corrigindo importação de modulos

---
+++ ciencia-da-computacao-uma-visao-abrangente/compilador-sistema-codificacao/src/main/java/dev/bstk/sistemacodificacao/App.java
@@ -0,0 +1,11 @@
+package dev.bstk.sistemacodificacao;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Sistema de codificação!!");
+    System.out.println("Sistema de codificação!!");
+    System.out.println("Sistema de codificação!!");
+  }
+
+}
