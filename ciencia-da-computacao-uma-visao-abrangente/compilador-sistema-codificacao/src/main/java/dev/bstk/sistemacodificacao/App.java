6d1508bbcbbf7497d976aae9b97753eb1ad49dd5
Bruno <bsilva.se@gmail.com>
2023-09-12 16:58:28 -0300

    main - Correção layout combobox

--- ciencia-da-computacao-uma-visao-abrangente/compilador-sistema-codificacao/src/main/java/dev/bstk/sistemacodificacao/App.java
+++ ciencia-da-computacao-uma-visao-abrangente/compilador-sistema-codificacao/src/main/java/dev/bstk/sistemacodificacao/App.java
@@ -1,12 +1,30 @@
 package dev.bstk.sistemacodificacao;
 
-import dev.bstk.sistemacodificacao.conversor.ConversorBinario;
+import dev.bstk.sistemacodificacao.ui.Conversor;
+
+import javax.swing.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class App {
 
+  private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
   public static void main(String[] args) {
-    System.out.println("Texto: Sistema de codificação");
-    System.out.println("Binario: " + ConversorBinario.converteTextoParaBinario("Sistema de codificação"));
+    SwingUtilities.invokeLater(() -> {
+      try {
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+          if ("Metal".equals(info.getName())) {
+            UIManager.setLookAndFeel(info.getClassName());
+            break;
+          }
+        }
+
+        new Conversor();
+      } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+        LOGGER.log(Level.SEVERE, null, ex);
+      }
+    });
   }
 
 }
